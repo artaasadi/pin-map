a047790005e9f3eb74f0cae8a9d55e93e073265b
9731006 <9731006@ceit.local>
2021-11-11 01:19:44 +0330

    some more warming ups

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path="/" exact/>
+        </Routes>
+      </Router>
       <h1>HELLLOOO</h1>
-    </div>
+    </>
   );
 }
 
